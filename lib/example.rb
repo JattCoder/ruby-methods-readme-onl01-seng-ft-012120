ccc5cc075e5a9d891e510caf085c8f85484857d5
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-03-19 04:48:11 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,9 @@
 # Use this space to code along with the readme
 def say_hello_world_ten_times
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+10.times do
+  puts phrase
+end
 end
 
 say_hello_world_ten_times
